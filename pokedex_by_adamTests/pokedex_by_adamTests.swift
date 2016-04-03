13ce46f7fa62d7597c4923162059df99dd1466ed
AdamSzendrei <adamszendrei@adams-macbook-air.local>
2016-04-03 11:04:10 +0200

    Initial Commit

---
+++ pokedex_by_adamTests/pokedex_by_adamTests.swift
@@ -0,0 +1,36 @@
+//
+//  pokedex_by_adamTests.swift
+//  pokedex_by_adamTests
+//
+//  Created by Adam Szendrei on 03/04/16.
+//  Copyright © 2016 Adam Szendrei. All rights reserved.
+//
+
+import XCTest
+@testable import pokedex_by_adam
+
+class pokedex_by_adamTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
