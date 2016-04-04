9b6151fefdfa149b515cb525b9b3d0b8cc1101e4
AdamSzendrei <adamszendrei@Adams-MacBook-Air.local>
2016-04-04 18:58:02 +0200

    new cell

---
+++ pokedex_by_adam/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex_by_adam
+//
+//  Created by Adam Szendrei on 04/04/16.
+//  Copyright © 2016 Adam Szendrei. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ pokedex_by_adam/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  pokedex_by_adam
+//
+//  Created by Adam Szendrei on 03/04/16.
+//  Copyright © 2016 Adam Szendrei. All rights reserved.
+//
+
+import Foundation
